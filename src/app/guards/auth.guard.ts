4be987308a5247a9c0be5450a9f9274c55ed3445
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-09-30 12:03:38 -0300

    Autenticación y redireccionamiento
    Si ya tengo mi usuario registrado entonces ingresa a home en caso no tenga, muestra la pantalla principal

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { map } from "rxjs/operators";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
+  constructor(private AFauth: AngularFireAuth) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.AFauth.authState.pipe(
+      map((auth) => {
+        if (auth === undefined || auth === null) {
+          return false;
+        } else {
+          return true;
+        }
+        // console.log(auth);
+        // return false;
+      })
+    );
   }
-  
 }
