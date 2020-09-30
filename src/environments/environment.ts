089430c763eb6d1515a36e71617d7fb946f48d31
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-09-30 03:35:23 -0300

    Agregando firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7MEwO7YyJLU_nue55asfBgYHEhKWs5bM",
+  authDomain: "ekosmed-58431.firebaseapp.com",
+  databaseURL: "https://ekosmed-58431.firebaseio.com",
+  projectId: "ekosmed-58431",
+  storageBucket: "ekosmed-58431.appspot.com",
+  messagingSenderId: "928698046609",
+  appId: "1:928698046609:web:34bc5c540e58ba16230730",
+  measurementId: "G-Q63LQL7PXG",
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
