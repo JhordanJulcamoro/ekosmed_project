7ea03637e8ef862906b45ff5073dfc56af05aa77
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-09-30 12:34:37 -0300

    Redirección de autenticado -> home sin acceso a login
    Solución del problema "cuando esta logueado se dirige a pantalla loguin", eso no puede ser posible ya que se encuentra logueado, entonces se creo un guard nologin y se redireccionó a la pantalla home en caso de que una persona logueada ingrese por url "login"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./guards/auth.guard";
+import { NologinGuard } from "./guards/nologin.guard";
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
     path: "login",
     loadChildren: () =>
       import("./componentes/login/login.module").then((m) => m.LoginPageModule),
+    canActivate: [NologinGuard],
   },
 ];
 
