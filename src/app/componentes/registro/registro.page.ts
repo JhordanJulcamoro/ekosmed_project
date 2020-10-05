3f5ee09806032db7c8e8987f90d811434de16acb
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-05 10:26:30 -0300

    Registro de usuario ts
    Registro de un usuario en firebase usando email y password.
    Adicionando la función de si el usuario esta verificado redirigir a home, si no está verificado redirigir a la página de verificación de email.

--- src/app/componentes/registro/registro.page.ts
+++ src/app/componentes/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 @Component({
   selector: "app-registro",
   templateUrl: "./registro.page.html",
@@ -9,12 +10,29 @@ export class RegistroPage implements OnInit {
   email: string;
   password: string;
 
-  constructor() {}
+  constructor(private authSvc: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
-  onRegister(email, password) {
-    console.log("Email->", email);
-    console.log("Password->", password);
+  async onRegister(email, password) {
+    try {
+      const user = await this.authSvc.register(email, password);
+      if (user) {
+        console.log("User->", user);
+        const isVerified = this.authSvc.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
+  private redirectUser(isVerified: boolean): void {
+    if (isVerified) {
+      this.router.navigate(["home"]);
+    } else {
+      console.log("Redirigiendo a pantalla de verificacion de email");
+      //this.router.navigate(["verificar-email"]);
+    }
   }
 }
