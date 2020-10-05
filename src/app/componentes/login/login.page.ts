88e81185d847c1e6112e1b0fafd90331f2e5e932
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-05 10:46:04 -0300

    Login de usuario ts
    Login de un usuario usando autenticación en firebase comparando email y password.
    Login de un usuario con su cuenta de google.
    Adicionando la función de si el usuario esta verificado redirigir a home, si no está verificado redirigir a la página de verificación de email.

--- src/app/componentes/login/login.page.ts
+++ src/app/componentes/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 @Component({
   selector: "app-login",
   templateUrl: "./login.page.html",
@@ -8,14 +9,38 @@ import { Component, OnInit } from "@angular/core";
 export class LoginPage implements OnInit {
   email: string;
   password: string;
-  constructor() {}
+  constructor(private authSvc: AuthService, private router: Router) {}
 
   ngOnInit() {}
-  onLogin(email, password) {
-    console.log("Email->", email);
-    console.log("Password->", password);
+  async onLogin(email, password) {
+    try {
+      const user = await this.authSvc.login(email, password);
+      if (user) {
+        const isVerified = this.authSvc.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log("Error->", error);
+    }
   }
-  onLoginGoogle() {
-    console.log("Logueo con google");
+  async onLoginGoogle() {
+    try {
+      const user = await this.authSvc.loginGoogle();
+      if (user) {
+        const isVerified = this.authSvc.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log("Error->", error);
+    }
+  }
+
+  private redirectUser(isVerified: boolean): void {
+    if (isVerified) {
+      this.router.navigate(["main"]);
+    } else {
+      console.log("Redirigiendo a pantalla de verificacion de email");
+      //this.router.navigate(["verificar-email"]);
+    }
   }
 }
