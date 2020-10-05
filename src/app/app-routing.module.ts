ced2f729e1f49e89e7d983b0a9a07252ec1f139e
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-05 19:11:33 -0300

    Guard auth ts - redireccionamiento de autenticación
    Si no estoy autenticado redirecciona a login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,55 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },  {
-    path: 'login',
-    loadChildren: () => import('./componentes/login/login.module').then( m => m.LoginPageModule)
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
   {
-    path: 'main',
-    loadChildren: () => import('./componentes/main/main.module').then( m => m.MainPageModule)
+    path: "login",
+    loadChildren: () =>
+      import("./componentes/login/login.module").then((m) => m.LoginPageModule),
   },
   {
-    path: 'registro',
-    loadChildren: () => import('./componentes/registro/registro.module').then( m => m.RegistroPageModule)
+    path: "main",
+    loadChildren: () =>
+      import("./componentes/main/main.module").then((m) => m.MainPageModule),
+    canActivate: [AuthGuard],
   },
   {
-    path: 'verificar-email',
-    loadChildren: () => import('./componentes/verificar-email/verificar-email.module').then( m => m.VerificarEmailPageModule)
+    path: "registro",
+    loadChildren: () =>
+      import("./componentes/registro/registro.module").then(
+        (m) => m.RegistroPageModule
+      ),
   },
   {
-    path: 'recuperar-password',
-    loadChildren: () => import('./componentes/recuperar-password/recuperar-password.module').then( m => m.RecuperarPasswordPageModule)
+    path: "verificar-email",
+    loadChildren: () =>
+      import("./componentes/verificar-email/verificar-email.module").then(
+        (m) => m.VerificarEmailPageModule
+      ),
   },
-
+  {
+    path: "recuperar-password",
+    loadChildren: () =>
+      import("./componentes/recuperar-password/recuperar-password.module").then(
+        (m) => m.RecuperarPasswordPageModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
