0617bb9c8a6d9ee7611585f5576dace450fd915f
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-05 19:56:23 -0300

    Guard noauth ts - redireccionamiento de autenticacion
    Redireccionamiento en caso de que este logueado a main

--- src/app/guards/no-auth.guard.ts
+++ src/app/guards/no-auth.guard.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NoAuthGuard implements CanActivate {
+  constructor(private aFauth: AngularFireAuth, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.aFauth.authState.pipe(
+      map((auth) => {
+        if (auth === undefined || auth === null) {
+          return true;
+        } else {
+          this.router.navigate(["/main"]);
+          return false;
+        }
+      })
+    );
   }
-  
 }
