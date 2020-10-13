79a6cb65bb04c8b517ddcce14f80f0048b6e8b06
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-10-12 22:37:47 -0300

    Validações campo input e-mail da tela "recuperar senha"

--- src/app/componentes/recuperar-password/recuperar-password.module.ts
+++ src/app/componentes/recuperar-password/recuperar-password.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { RecuperarPasswordPageRoutingModule } from './recuperar-password-routing.module';
+import { RecuperarPasswordPageRoutingModule } from "./recuperar-password-routing.module";
 
-import { RecuperarPasswordPage } from './recuperar-password.page';
+import { RecuperarPasswordPage } from "./recuperar-password.page";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RecuperarPasswordPageRoutingModule
+    RecuperarPasswordPageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [RecuperarPasswordPage]
+  declarations: [RecuperarPasswordPage],
 })
 export class RecuperarPasswordPageModule {}
--- src/app/componentes/recuperar-password/recuperar-password.page.ts
+++ src/app/componentes/recuperar-password/recuperar-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 @Component({
@@ -7,13 +8,31 @@ import { AuthService } from "../../services/auth.service";
   styleUrls: ["./recuperar-password.page.scss"],
 })
 export class RecuperarPasswordPage implements OnInit {
-  email: string;
-  constructor(private authSvc: AuthService, private router: Router) {}
+  public fGroup: FormGroup;
+
+  constructor(
+    private fBuilder: FormBuilder,
+    private authSvc: AuthService,
+    private router: Router
+  ) {
+    this.fGroup = this.fBuilder.group({
+      email: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ),
+        ]),
+      ],
+    });
+  }
 
   ngOnInit() {}
-  async onResetPassword(email: string) {
+
+  async onResetPassword() {
     try {
-      await this.authSvc.resetPassword(email);
+      await this.authSvc.resetPassword(this.fGroup.get("email").value);
       this.authSvc.presentAlert("Sucesso", "O envio foi satisfatório.");
       this.router.navigate(["/login"]);
     } catch (error) {
