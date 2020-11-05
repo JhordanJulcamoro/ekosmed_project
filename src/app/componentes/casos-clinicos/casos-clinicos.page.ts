63f20c769742ecb3a8e25443765eac712d15078c
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-11-05 03:23:01 -0300

    Consumiendo Api Ejemplo en Casos Clínicos

--- src/app/componentes/casos-clinicos/casos-clinicos.page.ts
+++ src/app/componentes/casos-clinicos/casos-clinicos.page.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
 
 @Component({
-  selector: 'app-casos-clinicos',
-  templateUrl: './casos-clinicos.page.html',
-  styleUrls: ['./casos-clinicos.page.scss'],
+  selector: "app-casos-clinicos",
+  templateUrl: "./casos-clinicos.page.html",
+  styleUrls: ["./casos-clinicos.page.scss"],
 })
 export class CasosClinicosPage implements OnInit {
+  public question = "";
+  public users: User[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.chamarHttp();
   }
 
+  ngOnInit() {}
+
+  async chamarHttp() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    this.users = (await this.http.get(url).toPromise()) as User[];
+  }
 }
