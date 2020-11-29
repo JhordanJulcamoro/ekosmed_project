05e19f05d9abff9da882a6600d683bb4d73c8576
Jhordan Julcamoro <jhordanjulcamoro@outlook.es>
2020-11-29 11:04:45 -0300

    Commet iframeUrl

--- src/app/componentes/chatbot/chatbot.page.ts
+++ src/app/componentes/chatbot/chatbot.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-// import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 @Component({
   selector: "app-chatbot",
@@ -7,14 +6,6 @@ import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
   styleUrls: ["./chatbot.page.scss"],
 })
 export class ChatbotPage implements OnInit {
-  // iFrameUrl: SafeResourceUrl;
-
-  // constructor(private snaitizer: DomSanitizer) {
-  // this.iFrameUrl = this.snaitizer.bypassSecurityTrustResourceUrl(
-  //   "https://console.dialogflow.com/api-client/demo/embedded/62dcc198-5fca-4928-8b3a-de5fe78c1508"
-  // );
-  // }
-
   vidUrl: SafeResourceUrl;
 
   constructor(private domSatizer: DomSanitizer) {}
